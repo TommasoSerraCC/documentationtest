ca67e546a1f45846024f304f3c823fa275dd5c7c
Tommaso <t.serra996@gmail.com>
2023-11-08 02:50:31 +0100

    Initial import

---
+++ documentationtest/core.py
@@ -0,0 +1,9 @@
+""" Simple module to test read the docs documentation
+"""
+
+def daily_calories(bodyweight):
+    """ Takes in imput the bodyweight in 'kg' and returns
+        the recommended daily calories intake
+    """
+    return bodyweight*25
+
